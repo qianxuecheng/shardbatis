531a7d03db0fc0dd9532d6fd069417142865d018
qianxuecheng <qianxuecheng@qianxuechengdeMacBook-Pro.local>
2015-08-23 20:31:31 +0800

    first commit

---
+++ src/main/java/com/github/shardbatis/App.java
@@ -0,0 +1,13 @@
+package com.github.shardbatis;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
